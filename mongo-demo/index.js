e59267a7229445aabd9cfce6bd01bcedb79b3fbf
hashika <m.g.hashikamaduranga@gmail.com>
2019-10-27 23:42:57 +0530

    add data to data base

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -3,3 +3,48 @@ const mongoose= require('mongoose');
 mongoose.connect('mongodb://localhost/playground').
 then(()=>console.log("database connected")).
 catch(error=>console.error("error found :",error));
+
+const courseShema =new mongoose.Schema({
+    name:String,
+    author:String,
+    tags :[String],
+    date : {type:String,default:Date.now()},
+    isPublish:Boolean
+});
+
+async function createCourse(){
+
+    const Course = mongoose.model('Course',courseShema);
+
+    const course =new Course({
+        name:"angular js project",
+        author:"hashika",
+        tags :["angular","front end"],
+        isPublish :true
+    });
+
+    const result= await course.save();
+    console.log(result)
+}
+async function getCourse(){
+
+   const course=await Course
+   //.find({author:"hashika",isPublish:true})
+  // .find({price:{$gte :10,$lte :27}})
+
+  //start with hashika
+  .find({author :/^hashika/})
+
+  //end 77with maduranga
+  .find(author :/maduranga$/)
+
+  //contains Mosh
+  .find({author:/.*hashika*./i})
+   .limit(10)
+   .sort({name:1})
+   .select({name:1,tags:1});
+   console.log(course);
+}
+
+getCourse();
+
