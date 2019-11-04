34a5b493d98f3994513681e837d14f8ff3afe6c1
hashika <m.g.hashikamaduranga@gmail.com>
2019-11-04 11:57:36 +0530

    work with mongoose modeling relationships

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -5,7 +5,7 @@ then(()=>console.log("database connected")).
 catch(error=>console.error("error found :",error));
 
 const courseShema =new mongoose.Schema({
-    name:String,
+    name:{type:String,required:true},
     author:String,
     tags :[String],
     date : {type:String,default:Date.now()},
@@ -15,45 +15,53 @@ const Course = mongoose.model('Course',courseShema);
 async function createCourse(){
 
     const course =new Course({
-        name:"angular js project",
+        //name:"angular js project",
         author:"hashika",
         tags :["angular","front end"],
         isPublish :true
     });
 
-    const result= await course.save();
-    console.log(result)
-}
-async function getCourse(){
+    try{
+        await course.validate();
+        // const result= await course.save();
+        // console.log(result)
+    }
+    catch(ex){
+        console.log(ex) 
+    }
+    }
+// async function getCourse(){
 
-   const course=await Course
-   //.find({author:"hashika",isPublish:true})
-  // .find({price:{$gte :10,$lte :27}})
+//    const course=await Course
+//    //.find({author:"hashika",isPublish:true})
+//   // .find({price:{$gte :10,$lte :27}})
 
-  //start with hashika
-  .find({author :/^hashika/})
+//   //start with hashika
+//   .find({author :/^hashika/})
 
-  //end with maduranga
-  .find({author :/maduranga$/})
+//   //end with maduranga
+//   .find({author :/maduranga$/})
 
-  //contains Mosh
-  .find({author:/.*hashika*./i})
-   .limit(10)
-   .sort({name:1})
-   .select({name:1,tags:1});
-   console.log(course);
-}
+//   //contains Mosh
+//   .find({author:/.*hashika*./i})
+//    .limit(10)
+//    .sort({name:1})
+//    .select({name:1,tags:1});
+//    console.log(course);
+// }
 
 //getCourse();
-async function updateCourse(){
+// async function updateCourse(){
 
-    const course = await Course.findById(id);
-    if(!course) return;
+//     const course = await Course.findById(id);
+//     if(!course) return;
 
-    course.isPublish=true;
-    course.author ="author author";
+//     course.isPublish=true;
+//     course.author ="author author";
 
-    const result = await course.save();
-    console.log(result);
-}
-updateCourse('5a68fe2142ae6a6482c4c9cb');
+//     const result = await course.save();
+//     console.log(result);
+// }
+//updateCourse('5a68fe2142ae6a6482c4c9cb');
+
+createCourse();
---
+++ nodejs/population.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
+const Author = mongoose.model('Author', new mongoose.Schema({
+  name: String,
+  bio: String,
+  website: String
+}));
+
+const Course = mongoose.model('Course', new mongoose.Schema({
+  name: String,
+  author:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Author'
+  }
+  
+}));
+
+async function createAuthor(name, bio, website) { 
+  const author = new Author({
+    name, 
+    bio, 
+    website 
+  });
+
+  const result = await author.save();
+  console.log(result);
+}
+
+async function createCourse(name, author) {
+  const course = new Course({
+    name, 
+    author
+  }); 
+  
+  const result = await course.save();
+  console.log(result);
+}
+
+async function listCourses() { 
+  const courses = await Course
+    .find()
+    .populate('author','name -_id')
+    .populate('category','name')
+    .select('name');
+  console.log(courses);
+}
+
+//createAuthor('Mosh', 'My bio', 'My Website');
+
+createCourse('Node Course', '5dbfab9b4a04960670d196b5')
+
+// listCourses();
